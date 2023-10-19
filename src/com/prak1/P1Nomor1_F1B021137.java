157e057e94e0289107aaf706d1a82f7efcc349ca
Raflin24 <138386207+Raflin24@users.noreply.github.com>
2023-10-19 17:22:30 +0800

    P1Nomor1_F1B021137.java

---
+++ src/com/prak1/P1Nomor1_F1B021137.java
@@ -0,0 +1,25 @@
+package com.prak1;
+
+
+class Flora {
+    String nama;
+    int tinggi;
+    int jumlahDaun;
+}
+
+public class P1Nomor1_F1B021137 {
+    public static void main(String[] args) {
+        Flora mangga = new Flora();
+        mangga.nama = "Mangga";
+        mangga.tinggi = 500;
+        mangga.jumlahDaun = 2000;
+
+        String a = mangga.nama;
+        int b = mangga.tinggi;
+        int c = mangga.jumlahDaun;
+
+        System.out.println("Nama : "+ a);
+        System.out.println("Tinggi : " + b + "cm" );
+        System.out.println("Jumlah Daun : " + c + "daun");
+    }
+}  
