f96d5c75258b8a20fd78d12347702d58be05d86c
I Komang Krisna Dwipayana Wadhiesta <komangkrisnarl@gmail.com>
2023-10-19 16:45:47 +0800

    Add files via upload

---
+++ P1No1_F1B021050.java
@@ -0,0 +1,22 @@
+//Membuat attribut dari sebuah class dan buatkan objek
+package p1no1_f1b021050;
+class tanaman {
+    String nama; 
+    String warna;
+    String jenis;
+}
+
+public class P1No1_F1B021050 {
+    public static void main(String[] args) {
+    tanaman anggrek = new tanaman(); 
+
+    anggrek.nama= "Anggrek";
+    anggrek.warna= "Ungu";
+    anggrek.jenis= "Orchidaceae";
+        
+    System.out.println(anggrek.nama);
+    System.out.println(anggrek.warna);
+    System.out.println(anggrek.jenis);
+}
+}
+
