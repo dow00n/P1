e76663939c27e8fd2a8b32af7ccc881a30eb7bdc
dow00n <148226231+dow00n@users.noreply.github.com>
2023-10-19 16:41:28 +0800

    Add files via upload

---
+++ P1No2_F1B021076.java
@@ -0,0 +1,44 @@
+package com.prak1;
+
+class Flora1 {
+    String name;
+    int tinggi;
+    int jumlahDaun;
+    String jenis;
+    int berat;
+
+}
+
+
+
+public class P1No2_F1B021076 {
+    public static void main(String[] args) {
+        Flora1 mangga = new Flora1();
+        mangga.name = "Mangga";
+        mangga.tinggi = 500;
+        mangga.jumlahDaun = 2000;
+        mangga.jenis = "Monokotil";
+        mangga.berat = 2;
+
+        System.out.println("Nama : " + mangga.name);
+        System.out.println("Tinggi : " + mangga.tinggi + "cm");
+        System.out.println("Jumlah Daun : " + mangga.jumlahDaun + "daun");
+        System.out.println("Jenis : " + mangga.jenis);
+        System.out.println("Berat : " + mangga.berat + "ton");
+        System.out.println("\n");
+
+        Flora1 pisang = new Flora1();
+        pisang.name = "pisang";
+        pisang.tinggi = 400;
+        pisang.jumlahDaun = 50;
+        pisang.jenis = "Dikotil";
+        pisang.berat = 1;
+    
+        System.out.println("Nama : " + pisang.name);
+        System.out.println("Tinggi : " + pisang.tinggi + "cm");
+        System.out.println("Jumlah Daun : " + pisang.jumlahDaun + "daun");
+        System.out.println("Jenis : " + pisang.jenis);
+        System.out.println("Berat : " + pisang.berat + "ton");
+        System.out.println("\n");
+    }
+}
